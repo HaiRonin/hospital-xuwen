8fce8a71b33768d0428ac121a8926ad9eee3fccd
jlegend2011@126.com <jlegend2011@126.com>
2020-09-06 23:38:45 +0800

    微信支付

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/his/controller/PayController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/his/controller/PayController.java
@@ -3,7 +3,9 @@ package com.ruoyi.web.controller.his.controller;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.utils.pay.WeixinMessageUtil;
+import com.ruoyi.utils.pay.WeixinAppPayUtils;
 import com.ruoyi.utils.pay.WeixinPayUtils;
+import com.ruoyi.utils.pay.WxSignCode;
 import com.ruoyi.web.core.config.WechatConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,15 +32,22 @@ public class PayController extends BaseController {
 
     private static final Logger LOG = LoggerFactory.getLogger(PayController.class);
 
-    @PostMapping("/wechat/pay")
+    /**
+     * 微信APP支付
+     *
+     * @param orderPrice 订单金额
+     * @param request
+     * @return
+     */
+    @PostMapping("/wechat/apppay")
     @ResponseBody
-    public AjaxResult weixinPay(String orderId, HttpServletRequest request) {
+    public AjaxResult wxAppPay(Double orderPrice, HttpServletRequest request) {
 
-        orderId = "1";//订单ID
+        String orderId = new Date().getTime() + "_wxapp_" + WeixinAppPayUtils.createNoncestr(3);
 
         Map<String, String> wxPayParams = new HashMap<String, String>();// 里面的参数有用于微信的签名，不能随便添加参数
-        Double price = new Double(0.01 * 100);//支付金额 单位是分
-        String prepayid = WeixinPayUtils.getPrePayId(
+        Double price = new Double(orderPrice * 100);//支付金额 单位是分
+        String prepayid = WeixinAppPayUtils.getPrePayId(
                 "预约支付", orderId,
                 String.valueOf(price.longValue()));
 
@@ -47,7 +56,7 @@ public class PayController extends BaseController {
         wxPayParams
                 .put("appid",
                         WechatConfig.appId);
-        wxPayParams.put("noncestr", WeixinPayUtils.CreateNoncestr());
+        wxPayParams.put("noncestr", WeixinAppPayUtils.createNoncestr());
         wxPayParams.put("package", "Sign=WXPay");
         wxPayParams
                 .put("partnerid",
@@ -57,11 +66,52 @@ public class PayController extends BaseController {
                 Long.toString(new Date().getTime() / 1000));
 
         LOG.info(">>>>>>>>>>>>>>>>>>微信支付签名参数：" + wxPayParams);
-        String sign = WeixinPayUtils.WxSignCreate(wxPayParams);
+        String sign = WeixinAppPayUtils.WxSignCreate(wxPayParams);
         LOG.info(">>>>>>>>>>>>>>>>>>微信支付签名结果：" + sign);
 
         wxPayParams.put("sign", sign);
         wxPayParams.put("packageValue", "Sign=WXPay");
+
+        return AjaxResult.success("支付成功", wxPayParams);
+    }
+
+    /**
+     * 微信公众号支付
+     *
+     * @param orderPrice 订单金额
+     * @param request
+     * @return
+     */
+    @PostMapping("/wechat/pay")
+    @ResponseBody
+    public AjaxResult weixinPay(Double orderPrice, HttpServletRequest request) {
+
+        String orderId = new Date().getTime() + "_wx_" + WeixinAppPayUtils.createNoncestr(3);
+
+        WxSignCode sign = new WxSignCode();
+        //获取商户的配置参数
+        sign.setAppid(WechatConfig.appId);
+        sign.setMch_id(WechatConfig.mchid);
+        sign.setBody("预约支付");
+        sign.setNonce_str(WeixinPayUtils.createNoncestr());
+        sign.setOpenid("openId");
+        sign.setOut_trade_no(orderId);//解决一个订单多个详情发起多个支付，导致订单号重复问题
+        sign.setSpbill_create_ip("127.0.0.1");
+        sign.setTotal_fee(String.valueOf((orderPrice * 100)));
+        sign.setNotify_url(WechatConfig.baseUrl);
+//        sign.setAttach(phone);//传递电话号码，用于回调更新详情信息
+        String code = WeixinPayUtils.getWxUnifiedOrderParamsXML(sign);
+        String url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+        Map<String, String> jsonObject = WeixinPayUtils.httpRequest(WechatConfig.unifiedorderUrl, "POST", code);
+        String prepayid = jsonObject.get("prepay_id");
+        Map<String, String> wxPayParams = new HashMap<String, String>();
+        wxPayParams.put("appId", WechatConfig.appId);
+        wxPayParams.put("timeStamp", Long.toString(new Date().getTime()));
+        wxPayParams.put("nonceStr", WeixinPayUtils.createNoncestr());
+        wxPayParams.put("package", "prepay_id=" + prepayid);
+        wxPayParams.put("signType", "MD5");
+        wxPayParams.put("paySign", WeixinPayUtils.WxSignCreate(wxPayParams));
+        wxPayParams.put("prepayid", prepayid);
 
         return AjaxResult.success("支付成功", wxPayParams);
     }
