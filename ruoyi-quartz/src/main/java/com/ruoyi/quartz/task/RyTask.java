bba207b1e7132571adc54b264c75319aa89c426a
HaiRonin <wdwlp999@163.com>
2020-09-12 20:49:39 +0800

    用户新增openId字段

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
@@ -2,8 +2,9 @@ package com.ruoyi.quartz.task;
 
 import com.ruoyi.his.constant.HisBusinessTypeEnum;
 import com.ruoyi.his.remote.AbstractHisServiceHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import com.ruoyi.common.utils.StringUtils;
 
 /**
  * 定时任务调度测试
@@ -11,31 +12,34 @@ import com.ruoyi.common.utils.StringUtils;
  * @author ruoyi
  */
 @Component("ryTask")
-public class RyTask
-{
-    public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
-    {
-        System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
-    }
+public class RyTask {
 
-    public void ryParams(String params)
-    {
-        System.out.println("执行有参方法：" + params);
-    }
+    protected static Logger logger = LoggerFactory.getLogger(RyTask.class);
+//    public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
+//    {
+//        System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
+//    }
 
-    public void ryNoParams()
+//    public void ryParams(String params)
+//    {
+//        System.out.println("执行有参方法：" + params);
+//    }
+
+//    public void ryNoParams()
+//    {
+//        System.out.println("执行无参方法");
+//        for(HisBusinessTypeEnum businessTypeEnum : HisBusinessTypeEnum.values()  ){
+//            AbstractHisServiceHandler.servicesInstance(businessTypeEnum).autoScanningRefund();
+//        }
+//    }
+
+    public void refund()
     {
-        System.out.println("执行无参方法");
+
         for(HisBusinessTypeEnum businessTypeEnum : HisBusinessTypeEnum.values()  ){
+            logger.info("task.run.name={}.start",businessTypeEnum.getDesc());
             AbstractHisServiceHandler.servicesInstance(businessTypeEnum).autoScanningRefund();
-        }
-    }
-
-    public void refundParams()
-    {
-        System.out.println("执行无参方法");
-        for(HisBusinessTypeEnum businessTypeEnum : HisBusinessTypeEnum.values()  ){
-            AbstractHisServiceHandler.servicesInstance(businessTypeEnum).autoScanningRefund();
+            logger.info("task.run.name={}.end",businessTypeEnum.getDesc());
         }
     }
 }
