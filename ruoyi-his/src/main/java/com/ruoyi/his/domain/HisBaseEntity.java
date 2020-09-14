351ece4fe71c0d824dfc1a6cb580555fbe5ec050
jlegend2011@126.com <jlegend2011@126.com>
2020-09-14 23:27:06 +0800

    解决冲突

--- ruoyi-his/src/main/java/com/ruoyi/his/domain/HisBaseEntity.java
+++ ruoyi-his/src/main/java/com/ruoyi/his/domain/HisBaseEntity.java
@@ -15,40 +15,71 @@ import java.util.Map;
  */
 public class HisBaseEntity extends BaseEntity {
     private static final long serialVersionUID = 1L;
-    /** 支付状态 */
+
+    /**
+     * 返回信息
+     */
+    @Excel(name = "返回信息")
+    @ApiModelProperty(value = "返回信息", hidden = true)
+    private String resultMsg;
+
+    /**
+     * his接口返回信息
+     */
+    @Excel(name = "his接口返回码")
+    @ApiModelProperty(value = "his接口返回码", hidden = true)
+    private String resultCode;
+
+    /**
+     * 支付状态
+     */
     @Excel(name = "支付状态")
+    @ApiModelProperty(value = "支付状态", hidden = true)
     private String successfulPayment;
 
-    /** 支付交易流水号 */
-    @Excel(name = "支付交易流水号")
+    /**
+     * 支付订单号
+     */
+    @Excel(name = "支付订单号")
+    @ApiModelProperty(value = "支付订单号", hidden = true)
     private String outTradeNo;
 
-    /** 微信订单号 */
-    @Excel(name = "微信订单号")
+    /**
+     * 支付流水订单号
+     */
+    @Excel(name = "支付流水号")
+    @ApiModelProperty(value = "支付流水号", hidden = true)
     private String transactionId;
 
     /***
      * 枚举类：HisPayType
      */
-    /** 支付类型 */
+    /**
+     * 支付类型
+     */
     @Excel(name = "支付类型")
+    @ApiModelProperty(value = "支付类型")
     private String payType;
 
     /**
      * 交易金额
      */
+    @ApiModelProperty(value = "交易金额", hidden = true)
     private BigDecimal amount;
 
     /***
      * 订单类型
      * 枚举类型：HisBusinessTypeEnum
      */
+    @ApiModelProperty(value = "订单类型", hidden = true)
     private String orderType;
 
     //预支付签名参数
+    @ApiModelProperty(value = "预支付签名参数", hidden = true)
     private Map<String, String> prePaySign;
 
     //用户OPENID
+    @ApiModelProperty(value = "用户OPENID", hidden = true)
     private String openId;
 
     public String getSuccessfulPayment() {
@@ -97,6 +128,22 @@ public class HisBaseEntity extends BaseEntity {
 
     public void setOrderType(String orderType) {
         this.orderType = orderType;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
     }
 
     public String getOpenId() {
