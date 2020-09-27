72f323592434e7589231995f0713a831482e8dcf
jlegend2011@126.com <jlegend2011@126.com>
2020-09-27 23:41:47 +0800

    微信ocr识别

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/his/api/HisHealthCardApi.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/his/api/HisHealthCardApi.java
@@ -6,7 +6,6 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.common.utils.ServletUtils;
-import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.his.remote.HealthCardService;
 import com.ruoyi.his.remote.request.healthcard.DynamicQRCodeResquest;
 import com.ruoyi.his.remote.request.healthcard.RegisterResquest;
@@ -22,14 +21,17 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ *
+ *
  * @author whl
  * @date 2020-08-08
  */
 @RestController
-@Api(value = "His健康卡接口", tags = {"His健康卡接口"})
+@Api(value="His健康卡接口",tags={"His健康卡接口"})
 @RequestMapping("/his/healthCard")
 @CrossOrigin
-public class HisHealthCardApi extends BaseController {
+public class HisHealthCardApi extends BaseController
+{
     @Autowired
     private HealthCardService healthCardService;
     @Autowired
@@ -37,27 +39,26 @@ public class HisHealthCardApi extends BaseController {
     /**
      * 2020.8.26
      * 测试预约挂号推his
-     * {
-     * "address": "深圳龙华新区白石龙",
-     * "adminExt": "wdwlp999@163.com",
-     * "birthday": "1984-12-06",
-     * "gender": "男",
-     * "idNumber": "450922198412064615",
-     * "idType": "01",
-     * "isCheck": 0,
-     * "name": "吴海浪",
-     * "nation": "汉族",
-     * "phone1": "15919865119",
-     * "wechatCode": "071FoDkl2sDEA54N8enl2HLUYD3FoDkn"
+     *{
+     *   "address": "深圳龙华新区白石龙",
+     *   "adminExt": "wdwlp999@163.com",
+     *   "birthday": "1984-12-06",
+     *   "gender": "男",
+     *   "idNumber": "450922198412064615",
+     *   "idType": "01",
+     *   "isCheck": 0,
+     *   "name": "吴海浪",
+     *   "nation": "汉族",
+     *   "phone1": "15919865119",
+     *   "wechatCode": "071FoDkl2sDEA54N8enl2HLUYD3FoDkn"
      * }
-     *
      * @return
      */
     @Log(title = "his本地调用", businessType = BusinessType.HIS_LOCALHOST)
     @ApiOperation("注册健康码")
     @ResponseBody
     @PostMapping(value = "/registerHealthCard")
-    public AjaxResult registerHealthCard(@RequestBody RegisterResquest registerResquest) {
+    public AjaxResult registerHealthCard(@RequestBody RegisterResquest registerResquest){
 //        if(StringUtils.isEmpty(registerResquest.getSmsCode())){
 //            return AjaxResult.error("请输入验证码");
 //        }
@@ -78,7 +79,7 @@ public class HisHealthCardApi extends BaseController {
     @ApiOperation("获取健康码")
     @ResponseBody
     @GetMapping(value = "/getHealthCard")
-    public AjaxResult getHealthCard(@RequestParam("healthCode") String healthCode) {
+    public AjaxResult getHealthCard(@RequestParam("healthCode") String healthCode){
         CardGetResponse response = healthCardService.getHealthCardByHealthCode(healthCode);
         return AjaxResult.success(JSON.toJSONString(response));
     }
@@ -93,14 +94,12 @@ public class HisHealthCardApi extends BaseController {
     @ApiOperation("获取健康卡二维码接口")
     @ResponseBody
     @PostMapping(value = "/getDynamicQRCode")
-    public AjaxResult registerHealthCard(@RequestBody DynamicQRCodeResquest dynamicQRCodeResquest) {
+    public AjaxResult registerHealthCard(@RequestBody DynamicQRCodeResquest dynamicQRCodeResquest){
         DynamicQRCodeResponse response = healthCardService.getDynamicQRCode(dynamicQRCodeResquest);
         return AjaxResult.success(JSON.toJSONString(response));
     }
 
     /**
-
-
      * 验证码短信
      *
      * @return
@@ -115,6 +114,7 @@ public class HisHealthCardApi extends BaseController {
         String msg ="【电子居民健康卡】%1$s为您的登录验证码，请于十分钟内填写。如非本人操作，请忽略本短信。";
         return smsService.sendVerificationCode(msg,phone);
     }
+
     /**
      * 2020.9.26
      * 身份证照片OCR接口
