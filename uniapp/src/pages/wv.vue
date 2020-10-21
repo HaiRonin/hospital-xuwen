b9f26fa8bbb98d9f5fe8fa06d03bfe0f41754ab2
zkp442910864 <442910864@qq.com>
2020-10-21 21:25:44 +0800

    设置ios打包，以及app启动协议

--- uniapp/src/pages/wv.vue
+++ uniapp/src/pages/wv.vue
@@ -12,7 +12,7 @@
         src = '';
 
         async onLoad (options: IOBJ) {
-            utils.showLoad();
+            utils.showLoad('支付中');
 
             // console.log(options);
             // console.log(options.url);
@@ -33,6 +33,7 @@
             const index = url.indexOf('?');
             const newUrl = url.substr(0, index);
             const strData = encodeURIComponent(url.substr(index + 5));
+            console.log(strData);
             this.src = `${newUrl}?url=${strData}`;
             // #endif
         }
@@ -41,21 +42,32 @@
 
         mounted () {
             // #ifdef APP-PLUS
-            this.$nextTick(() => {
-                var currentWebview = (this as any).$scope.$getAppWebview(); // 此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效，非v3编译模式使用this.$mp.page.$getAppWebview()
-                var wv = currentWebview.children()[0];
-                wv.addEventListener('loaded', (e: IOBJ) => {
-                    const url = wv.getURL();
-                    if (~url.indexOf('/pages/wvCallBack')) {
-                        // console.log(url);
-                        utils.link(1);
-                    }
-                }, false);
-            });
+            setTimeout(() => {
+                this.$nextTick(() => {
+                    var currentWebview = (this as any).$scope.$getAppWebview(); // 此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效，非v3编译模式使用this.$mp.page.$getAppWebview()
+                    var wv = currentWebview.children()[0];
+                    // console.log(currentWebview.children());
+                    // console.log(wv);
+
+                    if (!wv) return;
+                    wv.addEventListener('loaded', (e: IOBJ) => {
+                        const url = wv.getURL();
+                        console.log(url);
+                        if (~url.indexOf('/pages/wvCallBack')) {
+                            // console.log(url);
+                            utils.link(1);
+                        }
+                    }, false);
+                });
+            }, 500);
             // #endif
         }
 
         activated () {}
+
+        beforeDestroy () {
+            utils.hideLoad();
+        }
 
     }
 </script>
